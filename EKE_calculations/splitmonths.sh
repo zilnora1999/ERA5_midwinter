c209787ab150d962532a0dc59e8ace68401ab696
zilnora <zilnora@ethz.ch>
2023-09-08 11:10:51 +0200

    added notebook

--- EKE_calculations/splitmonths.sh
+++ EKE_calculations/splitmonths.sh
@@ -1,4 +1,6 @@
+PATH=/net/ch4/atmcirc/zilnora/era5_midwinter/data/EKE_calculations/
+
 for year in {1980..2022}
 do
-    cdo splitmon $year/EKE_highpass_$year $year/EKE_$year
+    cdo splitmon $PATH$year/EKE_highpass_$year $PATH$year/EKE_$year
 done
