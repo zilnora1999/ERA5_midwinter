77123589751b459588b9b904f6bde74d2fcf2745
zilnora <zilnora@ethz.ch>
2023-09-08 14:00:07 +0200

    changed EKE_plotting and added parallelisation

--- EKE_calculations/splitmonths.sh
+++ EKE_calculations/splitmonths.sh
@@ -1,6 +1,7 @@
+#-P 8 paralellises
 PATH=/net/ch4/atmcirc/zilnora/era5_midwinter/data/EKE_calculations/
 
 for year in {1980..2022}
 do
-    cdo splitmon $PATH$year/EKE_highpass_$year $PATH$year/EKE_$year
+    cdo -P 8 splitmon $PATH$year/EKE_highpass_$year $PATH$year/EKE_$year
 done
